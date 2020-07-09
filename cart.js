0e34432c3dcf3acc6d909402ba79cba155b5a8a6
peterfeld <89661123000@mail.ru>
2020-07-09 20:38:54 +0300

    правки + Js

---
+++ cart.js
@@ -0,0 +1,22 @@
+var itemsGet = document.querySelectorAll(".catalog-get");
+var cartPopup = document.querySelector(".popup-cart");
+var cartClose = cartPopup.querySelector(".modal-close");
+var cartCloseForm = cartPopup.querySelector(".poput-cart-button-buy");
+
+for (let itemGet of itemsGet) {
+    itemGet.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        cartPopup.classList.add("modal-show");
+    });
+}
+
+
+cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+});
+
+cartCloseForm.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+});
---
+++ feedback.js
@@ -0,0 +1,59 @@
+var feedbackButton = document.querySelector(".feedback-button");
+var feedbackPopup = document.querySelector(".feedback");
+var feedbackClose = feedbackPopup.querySelector(".modal-close");
+var feedbackForm = feedbackPopup.querySelector(".feedback-form");
+var userName = feedbackPopup.querySelector(".feedback-user-name");
+var userEmail = feedbackPopup.querySelector(".feedback-user-email");
+var feedbackText = feedbackPopup.querySelector(".feedback-text");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("name");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+feedbackButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.add("modal-show");
+    userName.focus();
+
+    if (storage) {
+        userName.value = storage;
+        userEmail.focus();
+        } else {
+            userName.focus();
+        }
+});
+
+feedbackClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove("modal-show");
+    feedbackPopup.classList.remove("modal-error");
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+    if (!userName.value || !userEmail.value || !feedbackText) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove("modal-error");
+        feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+        feedbackPopup.classList.add("modal-error");
+    }
+        else {
+            if (isStorageSupport) {
+            localStorage.setItem("name", userName.value);    
+            }
+        }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (feedbackPopup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove("modal-show");
+        feedbackPopup.classList.remove("modal-error");
+        }
+    }
+});
